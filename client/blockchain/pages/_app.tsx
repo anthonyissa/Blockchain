a7af2352ae7968624dc8adf409c69a9205e01d1e
Anthony ISSA <anthony.issa@hotmail.fr>
2022-12-12 00:01:14 +0100

    Fixed NEXT bug

--- client/blockchain/pages/_app.tsx
+++ client/blockchain/pages/_app.tsx
@@ -1,13 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Blockchain } from '../blockchain/Model/Blockchain';
 import { initServer } from '../blockchain/Server/Server';
 
 export default function App({ Component, pageProps }: AppProps) {
 
   
-  const [blockchain, setBlockchain] = useState<Blockchain | null>(initServer());
+  const [blockchain, setBlockchain] = useState<Blockchain | null>(null);
 
-  return <Component {...pageProps} blockchain={blockchain}/>
+  useEffect(()=>{
+    setBlockchain(initServer())
+  }, [])
+
+  return blockchain && <Component {...pageProps} blockchain={blockchain}/>
 }
